96709373db6cfdba3503bb237ff8515d5821f083
Maithili-Londhe <londhemaithili99@gmail.com>
2026-08-14 14:57:15 +0530

    First Commit in main

---
+++ Demo1.js
@@ -0,0 +1,4 @@
+console.log("Welcome to Day to Practice Session");
+console.log("Welcome to Day to Practice Session");
+console.log("Welcome to Day to Practice Session");
+console.log("Welcome to Day to Practice Session");
